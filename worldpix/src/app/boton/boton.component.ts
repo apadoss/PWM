0af3e044731aef624414e7032fd19beb2b48d929
rosma <rosmarygp11@gmail.com>
2024-04-09 10:42:11 +0100

    modification on boton.component.ts

--- worldpix/src/app/boton/boton.component.ts
+++ worldpix/src/app/boton/boton.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-boton',
-  standalone: true,
-  imports: [],
   templateUrl: './boton.component.html',
-  styleUrl: './boton.component.css'
+  styleUrls: ['./boton.component.css']
 })
 export class BotonComponent {
 
+  @Output() clicked: EventEmitter<void> = new EventEmitter<void>();
+
+  onClick(): void {
+    this.clicked.emit();
+  }
 }
