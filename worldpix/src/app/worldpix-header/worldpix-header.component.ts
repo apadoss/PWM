6844e587c3ca8dbcba0ccad965ecb467e8be56b8
Alejandro <apadoss03@gmail.com>
2024-04-26 21:52:12 +0100

    Hamburger menu now works correctly.

--- worldpix/src/app/worldpix-header/worldpix-header.component.ts
+++ worldpix/src/app/worldpix-header/worldpix-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { WorldpixLogoComponent } from '../worldpix-logo/worldpix-logo.component';
 import { HamburgerMenuComponent } from "./hamburger-menu/hamburger-menu.component";
 import { NgIf } from '@angular/common';
@@ -12,10 +12,12 @@ import { NgIf } from '@angular/common';
 })
 export class WorldpixHeaderComponent {
   showMenu = false;
+  @Input() image = "assets/hamburger-menu.png";
 
   constructor() { }
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+    this.image = this.showMenu ? "assets/close.png" : "assets/hamburger-menu.png";
   }
 }
