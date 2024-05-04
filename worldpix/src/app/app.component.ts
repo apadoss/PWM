446fdf3a800b02684f626f62a707b018219f947f
rosma <rosmarygp11@gmail.com>
2024-05-05 00:58:47 +0100

    sidebar change implemented

--- worldpix/src/app/app.component.ts
+++ worldpix/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
 import { TransparentButtonComponent } from './components/buttons/transparent-button/transparent-button.component';
 import { GenericButtonComponent } from './components/buttons/generic-button/generic-button.component';
 import { SidebarButtonComponent } from './components/buttons/sidebar-button/sidebar-button.component';
@@ -7,14 +9,15 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { Generic2ButtonComponent } from './components/buttons/generic2-button/generic2-button.component';
 import { MobileSidebarComponent } from './components/mobile-sidebar/mobile-sidebar.component';
-
+import { FinalSidebarComponent } from './final-sidebar/final-sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransparentButtonComponent, 
+  imports: [RouterOutlet, CommonModule, TransparentButtonComponent, 
     GenericButtonComponent, SidebarButtonComponent, 
     SidebarComponent, LoginFormComponent, 
-    Generic2ButtonComponent, MobileSidebarComponent],
+    Generic2ButtonComponent, MobileSidebarComponent,
+    FinalSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ worldpix/src/app/final-sidebar/final-sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, HostListener} from '@angular/core';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { MobileSidebarComponent } from '../components/mobile-sidebar/mobile-sidebar.component';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-final-sidebar',
+  standalone: true,
+  imports: [SidebarComponent, MobileSidebarComponent, CommonModule],
+  templateUrl: './final-sidebar.component.html',
+  styleUrl: './final-sidebar.component.css'
+})
+export class FinalSidebarComponent {
+  isLargeScreen: boolean = true; 
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkScreenSize() {
+    this.isLargeScreen = window.innerWidth > 730; 
+  }
+}
