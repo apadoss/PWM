c32b080816cbc431e6cf14786c951b3c0f058e80
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-05-04 15:06:34 +0100

    password validation

--- worldpix/src/app/components/register-form/register-form.component.ts
+++ worldpix/src/app/components/register-form/register-form.component.ts
@@ -13,7 +13,31 @@ export class RegisterFormComponent {
 
   constructor() { }
 
+  ngOnInit() {
+
+  }
+
   signUp(): void {
     alert('sign up pressed');
   }
+
+  validatePassword() {
+    var password = <HTMLInputElement>document.querySelector("input[name='passwordField']");
+    var confirmPassword = <HTMLInputElement>document.querySelector("input[name='confirmPassword']");
+    console.log("a")
+
+    if(!!password && !!confirmPassword && password.value != confirmPassword.value) {
+      if (confirmPassword.validationMessage === "Passwords don't match") {
+        confirmPassword.setCustomValidity('');
+      }  else {
+        confirmPassword.setCustomValidity("Passwords don't match");
+        confirmPassword.reportValidity();
+      }
+      } else {
+        confirmPassword.setCustomValidity('');
+      }
+    
+    //password.onchange = validatePassword;
+    //confirmPassword.onkeyup = validatePassword;
+}
 }
