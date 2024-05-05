b3f6359cddaecd4cd8fc11de0d937c88d6c1b843
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-05-05 17:21:35 +0100

    About us finished

--- worldpix/src/app/pages/about-us/about-us.component.ts
+++ worldpix/src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BodyComponent } from "../../components/structure/body/body.component";
+import { HeaderComponent } from "../../components/structure/header/header.component";
+import { FooterComponent } from "../../components/structure/footer/footer.component";
 
 @Component({
-  selector: 'app-about-us',
-  standalone: true,
-  imports: [],
-  templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+    selector: 'app-about-us',
+    standalone: true,
+    templateUrl: './about-us.component.html',
+    styleUrl: './about-us.component.css',
+    imports: [BodyComponent, HeaderComponent, FooterComponent]
 })
 export class AboutUsComponent {
 
