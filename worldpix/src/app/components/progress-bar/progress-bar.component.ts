de8603125189bf513a7697095b5f523f2abbb305
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2024-05-06 20:52:33 +0100

    load

--- worldpix/src/app/components/progress-bar/progress-bar.component.ts
+++ worldpix/src/app/components/progress-bar/progress-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [MatProgressBarModule],
+  imports: [MatProgressBarModule, MatProgressSpinnerModule],
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.css'
 })
