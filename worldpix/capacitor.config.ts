11f1c2e8472c8d8f5a2f5b2a6f0bfefcea57557a
Alejandro <apadoss03@gmail.com>
2024-05-19 16:45:25 +0100

    Added ionic and capacitor.

---
+++ worldpix/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.worldpix',
+  appName: 'worldpix',
+  webDir: 'src'
+};
+
+export default config;
